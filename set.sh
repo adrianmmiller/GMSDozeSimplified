43accc6a1e84f890339fa702fbd7ea3a1e280ff9
Adrian Miller <adrian.m.miller@gmail.com>
2024-01-01 04:31:28 +1100

    Delete set.sh

--- set.sh
+++
@@ -1,9 +0,0 @@
-git init --initial-branch=main
-sleep 1
-git remote add origin https://gitlab.com/adrian.m.miller/gmsdozesimplified.git
-sleep 1
-git add .
-sleep 1
-git commit -m "Initial commit"
-sleep 1
-git push --set-upstream origin main
